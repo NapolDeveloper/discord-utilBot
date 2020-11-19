bbb9fb36c4e5c68bcf6a08b8fbad0aede9d2c774
Kim Hyunjae <loma1016@gmail.com>
2020-11-20 01:35:32 +0900

    [추가] : 프로필 embed 틀 생성
    embed에 author avatar 추가해야됨.
    addFields를 이용해서 더 다양한 내용 추가.

--- app.js
+++ app.js
@@ -1,12 +1,20 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const Token = require('./token.json');
+
+// modules
+const Profile = require('./modules/profile/profile');
 client.on('ready', () => {
     console.log(`${client.user.tag}에 로그인하였습니다!`);
 });
 client.on('message', (msg) => {
-    if (msg.content === 'ping') {
-        msg.reply('pong');
+    if (msg.content.startsWith('!명령어')) {
+        msg.channel.send('test');
+        msg.react('d');
+    }
+    if (msg.content.startsWith('!프로필')) {
+        Profile.makeEmbed(msg.author.username, msg.author.id, msg.author.avatarURL); // 함수 호출로 임베드 생성
+        msg.reply(Profile.embed);
     }
 });
 client.login(Token.token);
---
+++ modules/profile/profile.js
@@ -0,0 +1,33 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+exports.makeEmbed = function (userName, userId, avatarUrl) {
+    const embed = new Discord.MessageEmbed()
+        .setColor('#0099ff')
+        .setAuthor(userName)
+        .addFields({
+            name: 'test',
+            value: userId,
+            inline: true,
+        })
+        .setThumbnail(avatarUrl);
+
+    /*
+        .setTitle(userName)
+        .setURL('https://discord.js.org/')
+        .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
+        .setDescription('Some description here')
+        .setThumbnail('https://i.imgur.com/wSTFkRM.png')
+        .addFields(
+            { name: 'Regular field title', value: 'Some value here' },
+            { name: '\u200B', value: '\u200B' },
+            { name: 'Inline field title', value: 'Some value here', inline: true },
+            { name: 'Inline field title', value: 'Some value here', inline: true }
+        )
+        .addField('Inline field title', 'Some value here', true)
+        .setImage('https://i.imgur.com/wSTFkRM.png')
+        .setTimestamp()
+        .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
+        */
+    exports.embed = embed;
+};
