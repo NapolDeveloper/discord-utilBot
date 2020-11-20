c133d4e4839507a79f35ebd27de38b03d3a87eda
Ara Developer <loma1016@gmail.com>
2020-11-20 09:53:03 +0900

    [추가] : 임베드 추가
    임베드에 내용 추가
    - userNickname
    - userId
    - userTag
    - userProfileImage

--- modules/profile/profile.js
+++ modules/profile/profile.js
@@ -1,18 +1,26 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 
-exports.makeEmbed = function (userName, userId, avatarUrl) {
-    const embed = new Discord.MessageEmbed()
-        .setColor('#0099ff')
-        .setAuthor(userName)
-        .addFields({
-            name: 'test',
-            value: userId,
-            inline: true,
-        })
-        .setImage(avatarUrl);
+exports.makeEmbed = function (userNickname, userId, userTag, avatarUrl) {
+  const embed = new Discord.MessageEmbed()
+    .setColor('#0099ff')
+    .setAuthor(userNickname + '의 프로필')
+    .addField((name = '서버 닉네임'), (value = userNickname))
+    .addFields(
+      {
+        name: '유저 ID',
+        value: userId,
+        inline: true,
+      },
+      {
+        name: '유저 Tag',
+        value: userTag,
+        inline: true,
+      }
+    )
+    .setThumbnail(avatarUrl);
 
-    /*
+  /*
         .setTitle(userName)
         .setURL('https://discord.js.org/')
         .setAuthor('Some name', 'https://i.imgur.com/wSTFkRM.png', 'https://discord.js.org')
@@ -29,5 +37,5 @@ exports.makeEmbed = function (userName, userId, avatarUrl) {
         .setTimestamp()
         .setFooter('Some footer text here', 'https://i.imgur.com/wSTFkRM.png');
         */
-    exports.embed = embed;
+  exports.embed = embed;
 };
