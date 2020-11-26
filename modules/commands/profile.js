f5ff0d4fffe71770b8001b51d6d46289da6a6d5e
Ara Developer <loma1016@gmail.com>
2020-11-27 07:08:06 +0900

    [수정] : 모듈화
    app.js에서 데이터를 받아오는 형식을 모듈 자체에서 구하는 형식으로 변경.

--- modules/commands/profile.js
+++ modules/commands/profile.js
@@ -1,29 +1,31 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 module.exports = {
-    name: '프로필',
-    description: 'test',
-    execute(message, args) {
-        const { guild } = message;
-        const user = message.mentions.users.first() || message.member.user;
-        const member = guild.members.cache.get(user.id);
-        const embed = new Discord.MessageEmbed()
-            .setColor('#0099ff')
-            .setAuthor(member.nickname + '의 프로필')
-            .addField((name = '서버 닉네임'), (value = member.nickname))
-            .addFields(
-                {
-                    name: '유저 ID',
-                    value: message.author.id,
-                    inline: true,
-                },
-                {
-                    name: '유저 Tag',
-                    value: user.tag,
-                    inline: true,
-                }
-            )
-            .setThumbnail(message.author.displayAvatarURL());
-        message.channel.send(embed);
-    },
+  name: '프로필',
+  description: 'test',
+  execute(message, args) {
+    const { guild } = message;
+    // const { name, region, memberCount, owner, afkTimeout } = guild;
+
+    const user = message.mentions.users.first() || message.member.user;
+    const member = guild.members.cache.get(user.id);
+    const embed = new Discord.MessageEmbed()
+      .setColor('#0099ff')
+      .setAuthor(member.nickname + '의 프로필')
+      .addField((name = '서버 닉네임'), (value = member.nickname))
+      .addFields(
+        {
+          name: '유저 ID',
+          value: message.author.id,
+          inline: true,
+        },
+        {
+          name: '유저 Tag',
+          value: user.tag,
+          inline: true,
+        }
+      )
+      .setThumbnail(message.author.displayAvatarURL());
+    message.channel.send(embed);
+  },
 };
